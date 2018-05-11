361a443a98fd4788e8a093793b95b848d472343d
linzhiqiang <844145131@qq.com>
2018-05-11 10:31:14 +0800

    add login

--- src/reducers/root.js
+++ src/reducers/root.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux-immutablejs';
 import auth from './auth';
+import login from './login'
 
 const rootReducer = combineReducers({
 	auth,
+	login
 });
 export default rootReducer;
