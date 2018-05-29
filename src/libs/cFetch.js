b7d125b99066e8abadca14019652d5eb75af6bbd
guifeng <fengyunlingying@qq.com>
2018-05-29 15:13:33 +0800

    aadd

--- src/libs/cFetch.js
+++ src/libs/cFetch.js
@@ -141,6 +141,7 @@ function toQueryString(object) {
 function cFetch(url, options, withAuth = true, defaultUrl = API_CONFIG.baseUri) {
     //let mergeUrl = API_CONFIG.baseUri + url;
     let mergeUrl = defaultUrl+url;
+    console.log(mergeUrl);
     const defaultOptions = {
         method: 'GET'
     };
