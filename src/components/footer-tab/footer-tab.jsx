3b625a247f8142bff1df1620de21bedd810fb9d1
guifeng <fengyunlingying@qq.com>
2018-05-29 10:26:15 +0800

    change

--- src/components/footer-tab/footer-tab.jsx
+++ src/components/footer-tab/footer-tab.jsx
@@ -7,34 +7,32 @@ class FooterTab extends Component {
 
     render() {
         return (
-            <div className='footer-tab'>
-                <ul className='footer'>
-                        <li className="">	
-                            <NavLink to='/mobile/home' activeClassName='active' className='a-icon'>
-                                <i className='icon-home-border'></i>
-                                <div>首页</div>
-                            </NavLink>   
-                        </li>
-                        <li className="">	
-                            <NavLink to='/mobile/subjectList' activeClassName='active' className='a-icon'>
-                                <i className='icon-invest-border'></i>
-                                <div>投资</div>
-                            </NavLink>
-                        </li>
-                        <li className="">	
-                            <NavLink to='/mobile/discover' activeClassName='active' className='a-icon'>
-                                <i className='icon-discovery-border'></i>
-                                <div>发现</div>
-                            </NavLink>
-                        </li>
-                        <li className="">	
-                            <NavLink to='/mobile/my' activeClassName='active' className='a-icon'>
-                                <i className='icon-mine-border'></i>
-                                <div>我的</div>
-                            </NavLink>
-                        </li>
-                </ul>
-            </div>
+            <ul className='footer'>
+                    <li className="">	
+                        <NavLink to='/mobile/home' activeClassName='active' className='a-icon'>
+                            <i className='icon-home-border'></i>
+                            <div>首页</div>
+                        </NavLink>   
+                    </li>
+                    <li className="">	
+                        <NavLink to='/mobile/subjectList' activeClassName='active' className='a-icon'>
+                            <i className='icon-invest-border'></i>
+                            <div>投资</div>
+                        </NavLink>
+                    </li>
+                    <li className="">	
+                        <NavLink to='/mobile/discover' activeClassName='active' className='a-icon'>
+                            <i className='icon-discovery-border'></i>
+                            <div>发现</div>
+                        </NavLink>
+                    </li>
+                    <li className="">	
+                        <NavLink to='/mobile/my' activeClassName='active' className='a-icon'>
+                            <i className='icon-mine-border'></i>
+                            <div>我的</div>
+                        </NavLink>
+                    </li>
+            </ul>
 
         );
     }
