936999112972121fdb74f967e08d6f3788b21408
linzhiqiang <844145131@qq.com>
2018-06-05 08:36:45 +0800

    修改端口号

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,5 @@
-const host = 'http://172.16.7.3:8020';
+//const host = 'http://172.16.7.3:8020';
+const host = 'http://47.95.39.231:8020';
 const baseUri = host + '/';
 export const API_CONFIG = {
   host: host,
