db2ffbd6dd586724897f8c5d1086a0be2839a9ba
linzhiqiang <844145131@qq.com>
2018-06-14 16:01:48 +0800

    修改风险评估

--- src/containers/authentication-page/authentication-page.jsx
+++ src/containers/authentication-page/authentication-page.jsx
@@ -44,7 +44,7 @@ class AuthenticationPage extends Component {
       sortBy:'',
       pageNum:'1',
       pageSize:'1000',
-      riskResultId:'',
+      riskResultId:this.props.match.params.id ? this.props.match.params.id : '',
       questionAndAnswerDtoList:array,
 
     }
