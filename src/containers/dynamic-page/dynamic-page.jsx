5983d8e7272705ec6e6efc40b8b94bf23d442828
fangcan <fangcan@baba88.com>
2018-06-29 10:18:45 +0800

    优化下拉刷新

--- src/containers/dynamic-page/dynamic-page.jsx
+++ src/containers/dynamic-page/dynamic-page.jsx
@@ -22,6 +22,9 @@ class DynamicPage extends Component {
       tabClassTwo: "",
       refreshing: false,
       height: document.documentElement.clientHeight,
+      hasMore: true,
+      down: false,
+      list: []
     };
   }
   handleClick(type, e) {
@@ -32,82 +35,111 @@ class DynamicPage extends Component {
       this.setState({
         borderClass: "one",
         tabClassOne: "active",
-        tabClassTwo: ""
+        tabClassTwo: "",
+        hasMore: true
       });
       this.getListData(2);
     } else {
       this.setState({
         borderClass: "two",
         tabClassOne: "",
-        tabClassTwo: "active"
+        tabClassTwo: "active",
+        hasMore: true
       });
       this.getListData(1);
     }
   }
   componentDidMount() {
-    ajaxData.pageNum=1;
+    ajaxData.pageNum = 1;
     const { dispatch } = this.props;
     dispatch(clearData());
-    this.getListData(2);
+    this.getListData(2).then(res => {
+      this.setState({
+        list: res.value.list
+      })
+    });
     const hei = this.state.height - ReactDOM.findDOMNode(this.ptr).offsetTop;
-		setTimeout(() => this.setState({
+    setTimeout(() => this.setState({
       height: hei,
     }), 0);
   }
+  componentWillReceiveProps(nextProps) {
+    const { dynamic } = nextProps;
+    console.log(nextProps)
+    if (ajaxData.pageNum == 1) {
+      this.setState({
+        list: dynamic.dynamic.list
+      })
+    } else {
+    }
+  }
   getListData(type) {
-    
+
     const { dispatch } = this.props;
     return dispatch(dynamic(type, ajaxData));
   }
   getNewData() {
     ajaxData.pageNum++;
-    if (this.state.borderClass == "one") {     
-      if(ajaxData.pageNum<this.props.dynamic.dynamic.pages || ajaxData.pageNum==this.props.dynamic.dynamic.pages){  
+
+    // this.setState({ refreshing: true });
+    if (this.state.borderClass == "one") {
+      if (ajaxData.pageNum < this.props.dynamic.dynamic.pages || ajaxData.pageNum == this.props.dynamic.dynamic.pages) {
         this.setState({
-          refreshing:true
-        })      
+          refreshing: true
+        })
         this.getListData(2, ajaxData)
-        .then(res=>{
-          this.setState({
-            refreshing:false
-          })   
-        })
-        .catch(err=>{
-          this.setState({
-            refreshing:false
-          }) 
-        })       
-      }else{
-        this.setState({
-          refreshing:false
-        }) 
-      }      
-    } else {
-       this.setState({
-          refreshing:true
-        })   
-      if(ajaxData.pageNum<this.props.dynamic.dynamic.pages || ajaxData.pageNum==this.props.dynamic.dynamic.pages){        
-        this.getListData(1, ajaxData)
-        .then(res=>{
-          this.setState({
-            refreshing:false
-          })  
-        })
-        .catch(err=>{
-          this.setState({
-            refreshing:false
+          .then(res => {
+            console.log(res)
+            this.setState({
+              // refreshing:false,
+              list: [...this.state.list, ...res.value.list]
+            })
+
           })
-        })
-      }else{
+          .catch(err => {
+            this.setState({
+              refreshing: false,
+              hasMore: false
+            })
+          })
+      } else {
         this.setState({
-          refreshing:false
+          refreshing: false,
+          hasMore: false,
+          down: true,
+        })
+      }
+    } else {
+      this.setState({
+        refreshing: true
+      })
+      if (ajaxData.pageNum < this.props.dynamic.dynamic.pages || ajaxData.pageNum == this.props.dynamic.dynamic.pages) {
+        this.getListData(1, ajaxData)
+          .then(res => {
+            this.setState({
+              // refreshing:false,
+              list: [...this.state.list, ...res.value.list]
+            })
+
+          })
+          .catch(err => {
+            this.setState({
+              refreshing: false,
+              hasMore: false
+            })
+          })
+      } else {
+        this.setState({
+          refreshing: false,
+          hasMore: false,
+          down: true,
         })
       }
     }
   }
   render() {
     const { dynamic } = this.props;
-    let list = this.props.dynamic.dynamic.list;
+    let list = this.state.list;
     return (
       <div className="dynamic-body">
         <div className="main">
@@ -131,35 +163,39 @@ class DynamicPage extends Component {
               ref={el => (this.ptr = el)}
               style={{ height: this.state.height, overflow: "auto" }}
               direction={"up"}
+              indicator={this.state.down ? { deactivate: '没有更多数据了' } : { deactivate: '上拉加载更多' }}
               refreshing={this.state.refreshing}
               onRefresh={this.getNewData.bind(this)}
             >
               {
-                (list)?
-                list.map(item => {
-                item.updateTime = item.updateTime.substring(0, 10);
-                return (
-                  <Link to={"/mobile/discDetail/" + item.id} key={item.id}>
-                    <dl className="dynamic-list">
-                      <dt className="l">
-                        <img src={item.affIcon || defaultImg} alt="" />
-                      </dt>
-                      <dd>
-                        <h3>{item.title}</h3>
-                        <p
-                          className="list-content"
-                          dangerouslySetInnerHTML={{ __html: item.affContent }}
-                        />
-                        <p className="time-p">
-                          <i className="icon-time" />&nbsp;&nbsp;&nbsp;发布时间：{
-                            item.updateTime
-                          }
-                        </p>
-                      </dd>
-                    </dl>
-                  </Link>
-                );
-              }):''
+                (list) ?
+                  list.map(item => {
+                    item.updateTime = item.updateTime.substring(0, 10);
+                    return (
+                      <Link to={"/mobile/discDetail/" + item.id} key={item.id}>
+                        <dl className="dynamic-list">
+                          <dt className="l">
+                            <img src={item.affIcon || defaultImg} alt="" />
+                          </dt>
+                          <dd>
+                            <h3>{item.title}</h3>
+                            <p
+                              className="list-content"
+                              dangerouslySetInnerHTML={{ __html: item.affContent }}
+                            />
+                            <p className="time-p">
+                              <i className="icon-time" />&nbsp;&nbsp;&nbsp;发布时间：{
+                                item.updateTime
+                              }
+                            </p>
+                          </dd>
+                        </dl>
+                      </Link>
+                    );
+                  }) : ''
+              }
+              {
+                this.state.hasMore ? <div></div> : <div>已经到底部了^-^```</div>
               }
             </PullToRefresh>
           </div>
