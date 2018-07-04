57bf2c0fee9204116aa262864f55b606334cb52a
linzhiqiang <844145131@qq.com>
2018-07-04 14:35:04 +0800

    修改端口

--- src/config/api.js
+++ src/config/api.js
@@ -3,11 +3,7 @@
 //dev
 // const host = 'http://172.16.7.3:8020';
 // 测试
-<<<<<<< .mine
 const host = 'http://47.95.39.231:8020';
-=======
-//const host = 'http://47.95.39.231:8020';
->>>>>>> .theirs
 const baseUri = host + '/';
 export const API_CONFIG = {
   host: host,
